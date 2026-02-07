843f050ec996b0a43946ef753f15a4cd916c9e4c
Jenil-diyora <diyorajenil23@gmail.com>
2026-02-07 11:54:45 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      'blacks-sie-layer-ment.trycloudflare.com'
-    ]
-  }
+  base: '/portfolio/', // Assumes repository name is 'portfolio'
 })
